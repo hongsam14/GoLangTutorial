72e6cb38c2e4558cc8627f8238f1310ceea96a53
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2024-04-09 18:28:05 +0900

    make hellos func

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -15,3 +15,23 @@ func	Hello(name string) (string, error) {
 	//message := fmt.Sprintf("Hi, %v. Welcome!!", name)
 	return message, nil
 }
+
+//Hellos returns a map that associate each of the named people
+func Hellos(names []string) (map[string]string, error) {
+	var (
+	     messages map[string] string
+	     message string
+	     err error
+	    )
+
+	messages = make(map[string]string)
+	for _, n := range names {
+		message, err = Hello(n)
+		//error check
+		if err != nil {
+			return nil, err
+		}
+		messages[n] = message
+	}
+	return messages, nil
+}
