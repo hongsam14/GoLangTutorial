c85f7a4e6c638828da04bcf4e9c882955de2be87
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2024-04-11 15:53:35 +0900

    make album module

---
+++ data-access/album.go
@@ -0,0 +1,14 @@
+package album
+
+type Album struct {
+	ID	int64
+	Title	string
+	Artist	string
+	Price	float32
+}
+
+func	albumByArtist(name string) ([]Album, error) {
+	//variables
+	var albums []Album
+	
+}
