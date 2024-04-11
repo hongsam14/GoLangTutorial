0559f7893036fa4a5e9c567a0c341685cc856db8
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2024-04-11 14:26:25 +0900

    connection between module & mysql

---
+++ data-access/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	//sql
+	"github.com/go-sql-driver/mysql"
+	)
+
+//package-level-variables
+
+/**
+  type DB struct{ ... }
+    func Open(driverName, dataSourceName string) (*DB, error)
+    func OpenDB(c driver.Connector) *DB
+  */
+var db_obj *sql.DB
+
+func	main() {
+	
+}
